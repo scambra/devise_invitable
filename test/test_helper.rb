c3a773a2186fb46b34d5174b684bf523e4ce30e7
Sergio Cambra <sergio@entrecables.com>
2009-12-09 01:58:57 +0100

    Fix url in mailer

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,9 +4,15 @@ require File.join(File.dirname(__FILE__), 'rails_app', 'config', 'environment')
 require 'test_help'
 require 'mocha'
 require File.join(File.dirname(__FILE__), '..', 'lib', 'devise', 'models', 'invitable')
+require File.join(File.dirname(__FILE__), '..', 'lib', 'devise', 'controllers', 'url_helpers')
+ActionView::Base.send :include, Devise::Controllers::UrlHelpers
+
+path = File.join(File.dirname(__FILE__), '..', 'app', 'views')
+ActionController::Base.view_paths << path
+DeviseMailer.view_paths << path
 
 ActionMailer::Base.delivery_method = :test
-ActionMailer::Base.perform_deliveries = true
+ActionMailer::Base.perform_deliveries = false
 ActionMailer::Base.default_url_options[:host] = 'test.com'
 
 ActiveRecord::Migration.verbose = false
@@ -14,8 +20,9 @@ ActiveRecord::Base.logger = Logger.new(nil)
 
 ActiveRecord::Schema.define(:version => 1) do
   create_table :users do |t|
-    t.authenticatable
+    t.authenticatable :null => true
     t.string :username
+    t.confirmable
     t.invitable
 
     t.timestamps
