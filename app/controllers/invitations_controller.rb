bf9f9eebe8b66b2f6451e57c9101821e59abde28
scambra <sergio@entrecables.com>
2010-02-12 17:37:32 +0100

    Revert dry controller, Devise::Controllers::Common has been removed

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,14 +1,19 @@
 class InvitationsController < ApplicationController
   include Devise::Controllers::InternalHelpers
-  include Devise::Controllers::Common
 
   before_filter :authenticate_resource!, :only => [:new, :create]
   before_filter :require_no_authentication, :only => [:edit, :update]
   helper_method :after_sign_in_path_for
 
+  # GET /resource/invitation/new
+  def new
+    build_resource
+    render_with_scope :new
+  end
+
   # POST /resource/invitation
   def create
-    self.resource = resource_class.send(send_instructions_with, params[resource_name])
+    self.resource = resource_class.send_invitation(params[resource_name])
 
     if resource.errors.empty?
       set_flash_message :notice, :send_instructions
@@ -35,10 +40,5 @@ class InvitationsController < ApplicationController
     else
       render_with_scope :edit
     end
-  end
-
-  protected
-  def send_instructions_with
-    :send_invitation
   end
 end
