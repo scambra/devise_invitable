3400fafdc9ae4ed6b32b33acfb37f2c477fa8dbc
Matt Hodgson <mhodgson@youcastr.com>
2010-06-02 18:56:26 -0400

    fix url helpers

--- lib/devise_invitable/controllers/url_helpers.rb
+++ lib/devise_invitable/controllers/url_helpers.rb
@@ -1,20 +1,24 @@
-Devise::Controllers::UrlHelpers.module_eval do
-  [:path, :url].each do |path_or_url|
-    [nil, :new_, :accept_].each do |action|
-      class_eval <<-URL_HELPERS
-        def #{action}invitation_#{path_or_url}(resource, *args)
-          resource = case resource
-            when Symbol, String
-              resource
-            when Class
-              resource.name.underscore
-            else
-              resource.class.name.underscore
-          end
+module DeviseInvitable
+  module Controllers
+    module UrlHelpers
+      [:path, :url].each do |path_or_url|
+        [nil, :new_, :accept_].each do |action|
+          class_eval <<-URL_HELPERS, __FILE__, __LINE__ + 1
+            def #{action}invitation_#{path_or_url}(resource, *args)
+              resource = case resource
+                when Symbol, String
+                  resource
+                when Class
+                  resource.name.underscore
+                else
+                  resource.class.name.underscore
+              end
 
-          send("#{action}\#{resource}_invitation_#{path_or_url}", *args)
+              send("#{action}\#{resource}_invitation_#{path_or_url}", *args)
+            end
+          URL_HELPERS
         end
-      URL_HELPERS
+      end
     end
   end
 end
