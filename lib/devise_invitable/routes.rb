6ee6abf8788bfdf43898013b440f4e4ff413dc24
Paul Rosania <paul.rosania@gmail.com>
2010-07-11 07:32:27 +0800

    Update route handling for Devise edge

--- lib/devise_invitable/routes.rb
+++ lib/devise_invitable/routes.rb
@@ -2,10 +2,8 @@ module ActionDispatch::Routing
   class Mapper
     protected
       def devise_invitation(mapping, controllers)
-        scope mapping.full_path[1..-1], :name_prefix => mapping.name do
-          resource :invitation, :only => [:new, :create, :update, :edit], :as => mapping.path_names[:invitation], :controller => controllers[:invitations]
-          # get :"accept_#{mapping.name}_invitation", :controller => 'invitations', :action => 'edit' # , :name_prefix => nil, :path_prefix => "#{mapping.name}/invitation"
-        end
+        resource :invitation, :only => [:new, :create, :edit, :update],
+                  :path => mapping.path_names[:invitation], :controller => controllers[:invitations]
       end
   end
 end
