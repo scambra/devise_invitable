986e9342c02619041bdeea2b6425576f2c08dec0
Sergio Cambra <sergio@entrecables.com>
2010-09-22 01:30:57 +0200

    Require devise/mailer before extending it

--- lib/devise_invitable/rails.rb
+++ lib/devise_invitable/rails.rb
@@ -5,6 +5,7 @@ module DeviseInvitable
     ActiveSupport.on_load(:action_view) { include DeviseInvitable::Controllers::UrlHelpers }
     
     config.after_initialize do
+      require 'devise/mailer'
       Devise::Mailer.send :include, DeviseInvitable::Mailer
     end
     
