e23c22143d61763333731154b5713848b632c89d
Denis Grankin <liquidautumn@gmail.com>
2011-09-24 01:53:35 +0300

    fixes deprecation warning 'DEPRECATION WARNING: Passing a boolean to authenticate_person! is deprecated, please use :force => true instead.'

--- lib/devise_invitable/controllers/helpers.rb
+++ lib/devise_invitable/controllers/helpers.rb
@@ -1,7 +1,7 @@
 module DeviseInvitable::Controllers::Helpers
   protected
   def authenticate_inviter!
-    send(:"authenticate_#{resource_name}!", true)
+    send(:"authenticate_#{resource_name}!", :force => true)
   end
 end
 ActionController::Base.send :include, DeviseInvitable::Controllers::Helpers
