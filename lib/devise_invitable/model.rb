5861d22d9842f275a4d6aee28f9d4d786da36273
scambra <sergio@entrecables.com>
2011-11-10 10:39:17 +0100

    add test about invitation_accepted_at attribute which was removed in previous commit

--- lib/devise_invitable/model.rb
+++ lib/devise_invitable/model.rb
@@ -33,7 +33,7 @@ module Devise
       def accept_invitation!
         if self.invited? && self.valid?
           self.invitation_token = nil
-          self.invitation_accepted_at = Time.now.utc # if has_attribute? :invitation_accepted_at
+          self.invitation_accepted_at = Time.now.utc if respond_to? :"invitation_accepted_at="
           self.save(:validate => false)
         end
       end
