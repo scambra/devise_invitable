def7b2996285e573c67d5a817fb5118ab1f75dbd
Gernot <g2011@brandyrox.com>
2012-03-20 23:55:43 +0100

    removed the overwriting create action of RegistrationsController and implemented the according logic with an around_filter instead

--- app/controllers/devise/registrations_controller.rb
+++ app/controllers/devise/registrations_controller.rb
@@ -1,30 +1,35 @@
 class Devise::RegistrationsController < Devise::RegistrationsController
-  # POST /resource
-  def create
-    build_resource
+  around_filter :destroy_if_previously_invited, :only => :create
 
-    resource = User.new(params[:user])
 
-    @user = User.find_by_email_and_encrypted_password(params[:user][:email], '')
-    if @user
-      @user.update_attributes(:password => params[:user][:password])
-      @user.invitation_token = nil
-      resource = @user
+  private
+
+  def destroy_if_previously_invited
+    invitation_info = {}
+
+    user_hash = params[:user]
+    if user_hash && user_hash[:email]
+      @user = User.find_by_email_and_encrypted_password(user_hash[:email], '')
+      if @user
+        invitation_info[:invitation_sent_at] = @user[:invitation_sent_at]
+        invitation_info[:invited_by_id] = @user[:invited_by_id]
+        invitation_info[:invited_by_type] = @user[:invited_by_type]
+        @user.destroy
+      end
     end
 
-    if resource.save
-      if resource.active_for_authentication?
-        set_flash_message :notice, :signed_up if is_navigational_format?
-        sign_in(resource_name, resource)
-        respond_with resource, :location => after_sign_up_path_for(resource)
-      else
-        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
-        expire_session_data_after_sign_in!
-        respond_with resource, :location => after_inactive_sign_up_path_for(resource)
-      end
-    else
-      clean_up_passwords resource
-      respond_with resource
+    # execute the action (create)
+    yield
+    # Note that the after_filter is executed at THIS position !
+
+    # Restore info about the last invitation (for later reference)
+    # Reset the invitation_info only, if invited_by_id is still nil at this stage:
+    @user = User.find_by_email_and_invited_by_id(user_hash[:email], nil)
+    if @user
+      @user[:invitation_sent_at] = invitation_info[:invitation_sent_at]
+      @user[:invited_by_id] = invitation_info[:invited_by_id]
+      @user[:invited_by_type] = invitation_info[:invited_by_type]
+      @user.save!
     end
   end
 end
