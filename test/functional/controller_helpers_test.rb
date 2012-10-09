07e98860f1cabb952f04ff36cdbc129f77ed97a6
Matthew Wear <matchematics@gmail.com>
2012-10-08 18:23:26 -0700

    Modified after invite/accept paths to work more like devise core.

---
+++ test/functional/controller_helpers_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class ControllerHelpersTest < ActionController::TestCase
+  tests ApplicationController
+
+  test "after invite path defaults to after sign in path" do
+    assert_equal @controller.after_sign_in_path_for(:user), @controller.after_invite_path_for(:user)
+  end
+  
+  test "after accept path defaults to after sign in path" do
+    assert_equal @controller.after_sign_in_path_for(:user), @controller.after_invite_path_for(:user)
+  end
+  
+  test 'after invite path is customizable from application controller' do
+    custom_path = 'customized/after/invite/path'
+    @controller.instance_eval "def after_invite_path_for(resource) '#{custom_path}' end"
+    assert_equal @controller.after_invite_path_for(:user), custom_path
+  end
+  
+  test 'after accept path is customizable from application controller' do
+    custom_path = 'customized/after/accept/path'
+    @controller.instance_eval "def after_accept_path_for(resource) '#{custom_path}' end"
+    assert_equal @controller.after_accept_path_for(:user), custom_path
+  end
+  
+  test 'is not a devise controller' do
+    assert !@controller.devise_controller?
+  end
+end
