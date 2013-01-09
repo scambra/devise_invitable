6c0993c70ddd124ec702beed46af39ae8e94f577
Toms Mikoss <toms.mikoss@gmail.com>
2013-01-09 11:08:23 +0200

    Change method signature of invitation_instructions mailer
    https://github.com/plataformatec/devise/commit/19b5bcbe0fe63489f67eaf42a
    efea382d45cc1d7 changes the signature in Devise side

--- lib/devise_invitable/mailer.rb
+++ lib/devise_invitable/mailer.rb
@@ -2,8 +2,8 @@ module DeviseInvitable
   module Mailer
 
     # Deliver an invitation email
-    def invitation_instructions(record)
-      devise_mail(record, :invitation_instructions)
+    def invitation_instructions(record, opts={})
+      devise_mail(record, :invitation_instructions, opts)
     end
   end
 end
