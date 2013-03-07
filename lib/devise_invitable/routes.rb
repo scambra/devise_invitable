bd8029bac6b90aaac28c4bebeaa1ab464837e99f
Niclas Vester <niclas.vester@gmail.com>
2013-03-07 19:54:21 +0100

    added some tests and refactored controller somewhat

--- lib/devise_invitable/routes.rb
+++ lib/devise_invitable/routes.rb
@@ -3,9 +3,10 @@ module ActionDispatch::Routing
 
   protected
     def devise_invitation(mapping, controllers)
-      resource :invitation, :only => [:new, :create, :update, :destroy],
+      resource :invitation, :only => [:new, :create, :update],
         :path => mapping.path_names[:invitation], :controller => controllers[:invitations] do
         get :edit, :path => mapping.path_names[:accept], :as => :accept
+        get :destroy, :path => mapping.path_names[:remove], :as => :remove
       end
     end
 
