e21d016664e20864d07a6e5d2b69e965b20aa868
Sergio Cambra <sergio@programatica.es>
2013-04-12 17:34:56 +0200

    fix scope

--- lib/devise_invitable/model.rb
+++ lib/devise_invitable/model.rb
@@ -41,7 +41,7 @@ module Devise
 
         attr_writer :skip_password
 
-        scope :active, lambda { where(:confirmation_token => nil) }
+        scope :active, lambda { where(:invitation_token => nil) }
         if defined?(Mongoid) && self < Mongoid::Document
           scope :invitation_not_accepted, lambda { where(:invitation_accepted_at => nil, :invitation_token.ne => nil) }
           scope :invitation_accepted, lambda { where(:invitation_accepted_at.ne => nil) }
