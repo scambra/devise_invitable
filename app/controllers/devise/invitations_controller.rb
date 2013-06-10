d12ff5f1b783cc085968bf7beaae10137112ee8e
Adrian Hooper <adrian@euphoricpanda.co.uk>
2013-06-10 13:29:44 -0700

    Resource find is now unscoped

--- app/controllers/devise/invitations_controller.rb
+++ app/controllers/devise/invitations_controller.rb
@@ -64,7 +64,7 @@ class Devise::InvitationsController < DeviseController
   end
   
   def resource_from_invitation_token
-    unless params[:invitation_token] && self.resource = resource_class.to_adapter.find_first(params.slice(:invitation_token))
+    unless params[:invitation_token] && self.resource = resource_class.unscoped.find_by_invitation_token(params[:invitation_token])
       set_flash_message(:alert, :invitation_token_invalid)
       redirect_to after_sign_out_path_for(resource_name)
     end
