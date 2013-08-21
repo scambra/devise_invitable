90364a7e7903dcaa2438311e782a441ca372387f
Sergio Cambra <sergio@programatica.es>
2013-08-21 22:05:31 +0200

    remove limit from token, fixes  #351

--- lib/generators/active_record/templates/migration.rb
+++ lib/generators/active_record/templates/migration.rb
@@ -1,7 +1,7 @@
 class DeviseInvitableAddTo<%= table_name.camelize %> < ActiveRecord::Migration
   def up
     change_table :<%= table_name %> do |t|
-      t.string     :invitation_token, :limit => 60
+      t.string     :invitation_token
       t.datetime   :invitation_created_at
       t.datetime   :invitation_sent_at
       t.datetime   :invitation_accepted_at
