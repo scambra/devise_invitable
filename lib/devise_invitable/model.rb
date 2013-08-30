95e69dbd6255f039ab00d4969352a52383b09cd5
Sergio Cambra <sergio@programatica.es>
2013-08-30 07:27:43 +0200

    make deliver_invitation public

--- lib/devise_invitable/model.rb
+++ lib/devise_invitable/model.rb
@@ -157,6 +157,12 @@ module Devise
         end
       end
 
+      # Deliver the invitation email
+      def deliver_invitation
+        self.update_attribute :invitation_sent_at, Time.now.utc unless self.invitation_sent_at
+        send_devise_notification(:invitation_instructions, @raw_invitation_token)
+      end
+
       protected
         # Overriding the method in Devise's :validatable module so password is not required on inviting
         def password_required?
@@ -165,12 +171,6 @@ module Devise
 
         def confirmation_required_for_invited?
           respond_to?(:confirmation_required?, true) && confirmation_required? && invitation_accepted?
-        end
-
-        # Deliver the invitation email
-        def deliver_invitation
-          self.update_attribute :invitation_sent_at, Time.now.utc unless self.invitation_sent_at
-          send_devise_notification(:invitation_instructions, @raw_invitation_token)
         end
 
         # Checks if the invitation for the user is within the limit time.
