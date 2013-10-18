dd50cbef572847180eed82aa7c9d50ec842b58bb
Sergio Cambra <sergio@programatica.es>
2013-10-18 10:21:18 -1000

    fix adding parameters to sanitizer, related to #380

--- lib/devise_invitable/parameter_sanitizer.rb
+++ lib/devise_invitable/parameter_sanitizer.rb
@@ -1,11 +1,26 @@
 module DeviseInvitable
   module ParameterSanitizer
     def invite
-      default_params.permit(resource_class.invite_key_fields)
+      default_params.permit self.for(:invite)
     end
 
     def accept_invitation
-      default_params.permit([:password, :password_confirmation, :invitation_token])
+      default_params.permit self.for(:accept_invitation)
+    end
+
+    def self.included(base)
+      base.alias_method_chain :attributes_for, :invitable
+    end
+
+    private
+    def attributes_for_with_invitable(kind)
+      case kind
+      when :invite
+        resource_class.invite_key_fields
+      when :accept_invitation
+        [:password, :password_confirmation, :invitation_token]
+      else attributes_for_without_invitable(kind)
+      end
     end
   end
 end
