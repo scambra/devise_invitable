245285732e350852c237394c042d3c0f4b0755cf
Sergio Cambra <sergio@programatica.es>
2013-12-23 08:52:06 +0100

    leave devise generate routes helpers, fixes #405

--- lib/devise_invitable/rails.rb
+++ lib/devise_invitable/rails.rb
@@ -2,10 +2,8 @@ module DeviseInvitable
   class Engine < ::Rails::Engine
 
     ActiveSupport.on_load(:action_controller) do
-      include DeviseInvitable::Controllers::UrlHelpers
       include DeviseInvitable::Controllers::Helpers
     end
-    ActiveSupport.on_load(:action_view)       { include DeviseInvitable::Controllers::UrlHelpers }
 
     # We use to_prepare instead of after_initialize here because Devise is a Rails engine; its
     # mailer is reloaded like the rest of the user's app.  Got to make sure that our mailer methods
