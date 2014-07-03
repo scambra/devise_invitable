ea75fd2f993390cd8fced1d1a1f74af548d8fa97
Dmitry Nikitin <dmitry.nop@gmail.com>
2014-07-03 19:48:15 +0700

    Fix incorrect rollback migration

--- lib/generators/active_record/templates/migration.rb
+++ lib/generators/active_record/templates/migration.rb
@@ -25,7 +25,7 @@ class DeviseInvitableAddTo<%= table_name.camelize %> < ActiveRecord::Migration
       t.remove_references :invited_by, :polymorphic => true
       t.remove :invitations_count, :invitation_limit, :invitation_sent_at, :invitation_accepted_at, :invitation_token, :invitation_created_at
     end
-    change_column_null    :users, :encrypted_password, false
+    change_column_null    :<%= table_name %>, :encrypted_password, false
 <% if class_name.constantize.columns_hash['password_salt'] -%>
     change_column_null :<%= table_name %>, :password_salt,false
 <% end -%>
