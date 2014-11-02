460e759e80519188efe16bd5fda5ff52f8480a00
Ezequiel Delpero <edelpero@gmail.com>
2014-11-02 17:16:33 -0300

    Moves yield around resource before checking if the invitation was successfully sent

--- app/controllers/devise/invitations_controller.rb
+++ app/controllers/devise/invitations_controller.rb
@@ -15,9 +15,11 @@ class Devise::InvitationsController < DeviseController
   # POST /resource/invitation
   def create
     self.resource = invite_resource
+    resource_invited = resource.errors.empty?
 
-    if resource.errors.empty?
-      yield resource if block_given?
+    yield resource if block_given?
+
+    if resource_invited
       if is_flashing_format? && self.resource.invitation_sent_at
         set_flash_message :notice, :send_instructions, :email => self.resource.email
       end
