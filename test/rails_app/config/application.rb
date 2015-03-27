851637abffbf60bd1841fa865651fc17a5d9ac49
Sergio Cambra <sergio@programatica.es>
2015-03-27 16:08:25 +0100

    fix tests

--- test/rails_app/config/application.rb
+++ test/rails_app/config/application.rb
@@ -20,6 +20,6 @@ module RailsApp
   class Application < Rails::Application
     config.filter_parameters << :password
     config.action_mailer.default_url_options = { :host => "localhost:3000" }
-    config.i18n.enforce_available_locales = true
+    config.i18n.enforce_available_locales = true if config.i18n.respond_to? :enforce_available_locales
   end
 end
