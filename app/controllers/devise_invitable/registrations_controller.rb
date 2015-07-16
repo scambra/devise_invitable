2171dd54feec9c4406b25cc93dd7a1c9b749ba56
Stan Low <a22301613@yahoo.com.tw>
2015-07-16 22:57:27 +0800

    Improving controller's coding style

--- app/controllers/devise_invitable/registrations_controller.rb
+++ app/controllers/devise_invitable/registrations_controller.rb
@@ -5,12 +5,19 @@ class DeviseInvitable::RegistrationsController < Devise::RegistrationsController
     hash ||= resource_params || {}
     if hash[:email]
       self.resource = resource_class.where(email: hash[:email]).first
-      if resource && resource.invited_to_sign_up?
+      if resource_invited_to_sign_up?
         resource.attributes = hash
         resource.send_confirmation_instructions if resource.confirmation_required_for_invited?
         resource.accept_invitation
       end
+    else
+      self.resource = super
     end
-    self.resource ||= super
+  end
+
+  private
+
+  def resource_invited_to_sign_up?
+    resource && resource.invited_to_sign_up?
   end
 end
