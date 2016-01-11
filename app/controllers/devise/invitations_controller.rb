d9613aeac27c1291ffece959f69c828016428806
Sergio Cambra <sergio@programatica.es>
2016-01-11 11:44:48 +0100

    wrap helper_method in if respond_to?, rails 5 API compatible, fixes #620

--- app/controllers/devise/invitations_controller.rb
+++ app/controllers/devise/invitations_controller.rb
@@ -4,7 +4,9 @@ class Devise::InvitationsController < DeviseController
   prepend_before_filter :has_invitations_left?, :only => [:create]
   prepend_before_filter :require_no_authentication, :only => [:edit, :update, :destroy]
   prepend_before_filter :resource_from_invitation_token, :only => [:edit, :destroy]
-  helper_method :after_sign_in_path_for
+  if respond_to? :helper_method
+    helper_method :after_sign_in_path_for
+  end
 
   # GET /resource/invitation/new
   def new
