c62bc05986fdad0aadad5cecdb94c380a8e5cfbf
Michael Wawra <michael@wawra.co.uk>
2016-07-25 21:04:40 +0100

    Yield `resource` before validity/acceptance checks
    In certain circumstances, I need to allow the user to do more than
    simply set a password when accepting an invite. A simple example is to
    allow the user to set their name etc. If this is required by the model
    validations, it currently breaks.
    
    By checking if for validation errors (`invitation_accepted`) after the
    yield, then we can use the block to complete the setup of the resource. This
    prevents the need to rewrite the controller in our own code, and we can
    do a lot more with the block.

--- app/controllers/devise/invitations_controller.rb
+++ app/controllers/devise/invitations_controller.rb
@@ -24,11 +24,10 @@ class Devise::InvitationsController < DeviseController
   # POST /resource/invitation
   def create
     self.resource = invite_resource
-    resource_invited = resource.errors.empty?
 
     yield resource if block_given?
 
-    if resource_invited
+    if resource.errors.empty?
       if is_flashing_format? && self.resource.invitation_sent_at
         set_flash_message :notice, :send_instructions, :email => self.resource.email
       end
@@ -53,11 +52,10 @@ class Devise::InvitationsController < DeviseController
   def update
     raw_invitation_token = update_resource_params[:invitation_token]
     self.resource = accept_resource
-    invitation_accepted = resource.errors.empty?
 
     yield resource if block_given?
 
-    if invitation_accepted
+    if resource.errors.empty?
       if Devise.allow_insecure_sign_in_after_accept
         flash_message = resource.active_for_authentication? ? :updated : :updated_not_active
         set_flash_message :notice, flash_message if is_flashing_format?
