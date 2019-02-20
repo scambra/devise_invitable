039cdfb7cc2839a5448210c8736eba1191f65666
jmstfv <github@jmstfv.com>
2019-02-20 21:44:04 +0400

    Enable Rails 6 migrations

--- lib/generators/active_record/devise_invitable_generator.rb
+++ lib/generators/active_record/devise_invitable_generator.rb
@@ -3,14 +3,14 @@ require 'rails/generators/active_record'
 module ActiveRecord
   module Generators
     class DeviseInvitableGenerator < ActiveRecord::Generators::Base
-      source_root File.expand_path("../templates", __FILE__)
+      source_root File.expand_path('../templates', __FILE__)
 
       def copy_devise_migration
-        migration_template "migration.rb", "db/migrate/devise_invitable_add_to_#{table_name}.rb", migration_version: migration_version
+        migration_template 'migration.rb', "db/migrate/devise_invitable_add_to_#{table_name}.rb", migration_version: migration_version
       end
 
       def migration_version
-        if Rails.version.start_with? '5.'
+        if Rails::VERSION::MAJOR >= 5
           "[#{Rails::VERSION::MAJOR}.#{Rails::VERSION::MINOR}]"
         end
       end
