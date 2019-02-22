0fef454d5813177be3e410f6d43d948ea3377b35
jmstfv <github@jmstfv.com>
2019-02-22 19:26:33 +0400

    Remove commented-out methods

--- lib/generators/devise_invitable/devise_invitable_generator.rb
+++ lib/generators/devise_invitable/devise_invitable_generator.rb
@@ -5,10 +5,6 @@ module DeviseInvitable
 
       desc 'Add :invitable directive in the given model. Also generate migration for ActiveRecord'
 
-      # def devise_generate_model
-      #   invoke "devise", [name]
-      # end
-
       def inject_devise_invitable_content
         path = File.join('app', 'models', "#{file_path}.rb")
         inject_into_file(path, 'invitable, :', after: 'devise :') if File.exists?(path)
