b189979ad2625d843a7c879329aa9b89ba16c2f0
Sergio Cambra <sergio@enpijama.es>
2024-10-16 16:53:53 +0200

    fix testing against rails 6.1 ignore rails 8 testing, probably devise is not ready

--- test/orm/active_record.rb
+++ test/orm/active_record.rb
@@ -1,6 +1,13 @@
 ActiveRecord::Migration.verbose = false
 ActiveRecord::Base.logger = Logger.new(nil)
 
-ActiveRecord::MigrationContext.new(
-  File.expand_path('../../rails_app/db/migrate/', __FILE__)
-).migrate
+if ActiveRecord::VERSION >= 7
+  ActiveRecord::MigrationContext.new(
+    File.expand_path('../../rails_app/db/migrate/', __FILE__)
+  ).migrate
+else # rails 6
+  ActiveRecord::MigrationContext.new(
+    File.expand_path('../../rails_app/db/migrate/', __FILE__),
+    ActiveRecord::Base.connection.schema_migration
+  ).migrate
+end
