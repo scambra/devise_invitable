8e434dd99567cf8bb19603bbf85386021293f5c3
Sergio Cambra <sergio@enpijama.es>
2024-10-16 16:58:05 +0200

    fix active record testing

--- test/orm/active_record.rb
+++ test/orm/active_record.rb
@@ -1,7 +1,7 @@
 ActiveRecord::Migration.verbose = false
 ActiveRecord::Base.logger = Logger.new(nil)
 
-if ActiveRecord::VERSION >= 7
+if ActiveRecord::VERSION::MAJOR >= 7
   ActiveRecord::MigrationContext.new(
     File.expand_path('../../rails_app/db/migrate/', __FILE__)
   ).migrate
